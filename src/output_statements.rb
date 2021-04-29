288de9ae58bdf572ad41e812cf9d29cf57e124db
Ardelean Adrian <ardelean.adrian.mihai@gmail.com>
2021-04-30 00:47:40 +0300

    Add boolean algebra

--- src/output_statements.rb
+++ src/output_statements.rb
@@ -19,8 +19,8 @@ class OutputStatements
     return { "P(#{node})": "#{inputs.first}(#{node}1)", "P'(#{node})": "/#{inputs.first}(#{node}0)" } if input?(gate)
 
     {
-      "P(#{node})": compose_data(node, gate, inputs, :'1'),
-      "P'(#{node})": compose_data(node, gate, inputs, :'0')
+      "P(#{node})": elaborate_literal_variable(node, gate, inputs, :'1'),
+      "P'(#{node})": elaborate_literal_variable(node, gate, inputs, :'0')
     }
   end
 
@@ -28,20 +28,52 @@ class OutputStatements
     gate.casecmp?('INPUT')
   end
 
-  def compose_data(node, gate, inputs, state)
+  def elaborate_literal_variable(node, gate, inputs, state)
     raw_expression = LogicGates.truth_table[gate.to_sym][state]
-    raw_expression.gsub!("A'", compose_false_expression(node, inputs.first, state))
-    raw_expression.gsub!("B'", compose_false_expression(node, inputs.last, state))
-    raw_expression.gsub!('A', compose_true_expression(node, inputs.first, state))
-    raw_expression.gsub!('B', compose_true_expression(node, inputs.last, state))
+    if raw_expression.include?(' ⋅ ')
+      right_argument, left_argument = raw_expression.split(' ⋅ ')
+      elaborate_right_argument(right_argument, node, inputs.first, state)
+      elaborate_left_argument(left_argument, node, inputs.last, state)
+
+      return apply_boolean_algebra(right_argument, left_argument)
+    end
+
+    elaborate_right_argument(raw_expression, node, inputs.first, state)
+    elaborate_left_argument(raw_expression, node, inputs.last, state)
     raw_expression
   end
 
-  def compose_true_expression(node, input, state)
+  def elaborate_right_argument(expression, node, input, state)
+    expression.gsub!("A'", elaborate_false_expression(node, input, state)) ||
+      expression.gsub!('A', elaborate_true_expression(node, input, state))
+  end
+
+  def elaborate_left_argument(expression, node, input, state)
+    expression.gsub!("B'", elaborate_false_expression(node, input, state)) ||
+      expression.gsub!('B', elaborate_true_expression(node, input, state))
+  end
+
+  def apply_boolean_algebra(right_argument, left_argument)
+    right_arguments = right_argument.split(' + ')
+    left_arguments = left_argument.split(' + ')
+
+    return "#{right_arguments.first} ⋅ #{left_arguments.first}" if right_arguments.size == 1 && left_arguments.size == 1
+
+    expression = ''
+    right_arguments.each do |r_argument|
+      left_arguments.each do |l_argument|
+        expression << r_argument + ' ⋅ ' + l_argument + ' + '
+      end
+    end
+
+    expression.delete_suffix(' + ')
+  end
+
+  def elaborate_true_expression(node, input, state)
     @statements.dig(input.to_sym, :"P(#{input})")&.gsub(')', ", #{node}#{state})")
   end
 
-  def compose_false_expression(node, input, state)
+  def elaborate_false_expression(node, input, state)
     @statements.dig(input.to_sym, :"P'(#{input})")&.gsub(')', ", #{node}#{state})")
   end
 end
