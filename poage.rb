2ce5e034b626f0d329ad674f5e131689c9f7916e
Ardelean Adrian <ardelean.adrian.mihai@gmail.com>
2021-05-02 23:27:30 +0300

    Read inputs from keyboard

--- poage.rb
+++ poage.rb
@@ -3,30 +3,32 @@ require_relative 'src/output_statements'
 require_relative 'src/solve_boolean_equation'
 require_relative 'src/utils/equation'
 
-if ARGV.size > 1
-  puts 'To many arguments. Require 0 or 1'
+if ARGV.empty?
+  puts 'Require at least 1 input'
   exit(1)
 end
 
-read_circuit = LogicCircuit.new(ARGV.empty? ? LogicCircuit::FILENAME : ARGV.first)
+read_circuit = LogicCircuit.new
 
 unless read_circuit.valid?
   puts read_circuit.errors
   exit(2)
 end
 
-logic_circuit = read_circuit.circuit
+possible_errors = read_circuit.nodes.map { |node| %W[#{node}0 #{node}1] }
+possible_errors.flatten!
+
+unless ARGV.all? { |possible_error| possible_errors.include?(possible_error) }
+  puts 'Invalid input'
+  exit(3)
+end
 
 output_statements_service = OutputStatements.new
-logic_circuit.each { |expression| output_statements_service.elaborate(expression) }
+read_circuit.circuit.each { |expression| output_statements_service.elaborate(expression) }
 
-last_node = output_statements_service.statements.keys.last
+last_node = read_circuit.nodes.last&.to_sym
 statements = output_statements_service.statements[last_node].values
 
-equation_service = Equation.new(['e0'], statements)
-boolean_equation = SolveBooleanEquation.new(['x1', 'x2', 'x3', 'x4'], equation_service.generate)
-puts boolean_equation.solve
-
-equation_service.defects = ['a1', 'd1']
-boolean_equation.equation = equation_service.generate
+equation_service = Equation.new(ARGV, statements)
+boolean_equation = SolveBooleanEquation.new(read_circuit.inputs, equation_service.generate)
 puts boolean_equation.solve
