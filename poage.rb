69641943dc6e1f511f679da863e43e7d8f0266c8
Adrian Ardelean <ardelean.adrian.mihai@gmail.com>
2021-05-02 20:22:42 +0300

    refactoring (#4)

--- poage.rb
+++ poage.rb
@@ -1,31 +1,32 @@
 require_relative 'src/logic_circuit'
 require_relative 'src/output_statements'
 require_relative 'src/solve_boolean_equation'
+require_relative 'src/utils/equation'
 
 if ARGV.size > 1
   puts 'To many arguments. Require 0 or 1'
   exit(1)
 end
 
-circuit_service = LogicCircuit.new(ARGV.empty? ? LogicCircuit::FILENAME : ARGV.first)
+read_circuit = LogicCircuit.new(ARGV.empty? ? LogicCircuit::FILENAME : ARGV.first)
 
-unless circuit_service.valid?
-  puts circuit_service.errors
+unless read_circuit.valid?
+  puts read_circuit.errors
   exit(2)
 end
 
-logic_circuit = circuit_service.circuit
-
-puts logic_circuit
+logic_circuit = read_circuit.circuit
 
 output_statements_service = OutputStatements.new
 logic_circuit.each { |expression| output_statements_service.elaborate(expression) }
 
-output_statements_service.statements.each_value { |literal_variable| puts literal_variable }
-
 last_node = output_statements_service.statements.keys.last
 statements = output_statements_service.statements[last_node].values
 
-boolean_equation = SolveBooleanEquation.new(['x1', 'x2', 'x3', 'x4'], ['e0'], statements.first, statements.last)
-boolean_equation.solve
-p boolean_equation.solutions
+equation_service = Equation.new(['e0'], statements)
+boolean_equation = SolveBooleanEquation.new(['x1', 'x2', 'x3', 'x4'], equation_service.generate)
+puts boolean_equation.solve
+
+equation_service.defects = ['a1', 'd1']
+boolean_equation.equation = equation_service.generate
+puts boolean_equation.solve
--- src/solve_boolean_equation.rb
+++ src/solve_boolean_equation.rb
@@ -1,86 +1,34 @@
 require_relative 'utils/inputs'
-require_relative 'utils/boolean_algebra'
 
 class SolveBooleanEquation
+  attr_writer :equation
 
-  attr_reader :solutions
-
-  def initialize(primary_inputs, defects, true_literal_variable, false_literal_variable)
-    @solutions = []
+  def initialize(primary_inputs, equation)
     @primary_inputs = primary_inputs
-    @defects = defects
-    @inverse_defects = reverse_defects
-    @true_literal_variable = true_literal_variable
-    @false_literal_variable = false_literal_variable
-    @true_statement_faultless = faultless_statement(true_literal_variable)
-    @false_statement_faultless = faultless_statement(false_literal_variable)
-    @true_statement_defect = defect_statement(true_literal_variable)
-    @false_statement_defect = defect_statement(false_literal_variable)
+    @equation = equation
   end
 
   def solve
     inputs = Inputs.generate(@primary_inputs)
-    equation = generate_equation
+    solutions = []
     inputs.each do |input|
-      input.keys.each do |key|
-        equation.gsub!("/#{key}", input[key].to_i.zero? ? '1' : '0')
-        equation.gsub!(key, input[key])
-      end
+      equation = replace_inputs(input, @equation)
 
       arguments = equation.split(' + ')
       arguments.map! { |argument| argument.include?('0') ? '0' : '1' }
       solutions << input if arguments.include?('1')
-      equation = generate_equation
     end
-  end
-
-  def defects=(value)
-    @defects = value
-    @inverse_defects = reverse_defects
-    @true_statement_defect = defect_statement(@true_literal_variable)
-    @false_statement_defect = defect_statement(@false_literal_variable)
+    solutions
   end
 
   private
 
-  def faultless_statement(literal_variable)
-    statement = String.new(literal_variable)
-    arguments = literal_variable.split(/ ⋅ | \+ /)
-    arguments.each do |argument|
-      primary_input, = argument.split(/[()]/)
-      statement.gsub!(argument, primary_input)
+  def replace_inputs(input, equation)
+    boolean_equation = String.new(equation)
+    input.keys.each do |key|
+      boolean_equation.gsub!("/#{key}", input[key].to_i.zero? ? '1' : '0')
+      boolean_equation.gsub!(key, input[key])
     end
-    statement
-  end
-
-  def defect_statement(literal_variable)
-    statement = String.new(literal_variable)
-    arguments = literal_variable.split(/ ⋅ | \+ /)
-    arguments.each do |argument|
-      primary_input, variables = argument.split(/[()]/)
-      variables = variables.gsub(' ', '').split(',').reverse!
-
-      apply_rules(statement, argument, primary_input, variables)
-    end
-    statement
-  end
-
-  def apply_rules(statement, argument, primary_input, variables)
-    variables.each do |variable|
-      statement.gsub!(argument, '1') if @defects.include?(variable)
-      statement.gsub!(argument, '0') if @inverse_defects.include?(variable)
-    end
-    statement.gsub!(argument, primary_input)
-  end
-
-  def reverse_defects
-    @defects.map { |defect| defect.include?('1') ? defect.gsub('1', '0') : defect.gsub('0', '1') }
-  end
-
-  def generate_equation
-    first_argument = BooleanAlgebra.apply(@false_statement_faultless, @true_statement_defect)
-    last_argument = BooleanAlgebra.apply(@true_statement_faultless, @false_statement_defect)
-
-    "#{first_argument} + #{last_argument}"
+    boolean_equation
   end
 end
